5789930895c4d30dd40bca988dce9c4b4f554463
epost <25180070+erpost@users.noreply.github.com>
2019-03-06 14:54:07 -0500

    commit

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import os
 dump_file = 'bq_dump.csv'
 distinct_file = 'bq_distinct.txt'
 distinct_set = set()
+dlp_findings_list = []
 dlp_findings = 'dlp_findings.csv'
 split_dir = './splits/'
+
 
 def get_key():
     storage_key = os.path.expanduser('~/.gcp/dlp-dev.json')
@@ -44,7 +46,7 @@ with open(dump_file, 'w', newline='') as dump_file:
                 if type(value.values()[0]) == str:
                     distinct_set.add(value.values()[0])
 
-# write distinct lines to text
+# write distinct lines to text file
 with open(distinct_file, 'w') as distinct_file:
     for item in distinct_set:
         distinct_file.write('{}\n'.format(item))
@@ -53,7 +55,7 @@ with open(distinct_file, 'w') as distinct_file:
 fs = FileSplit(file='bq_distinct.txt', splitsize=524288, output_dir='splits')
 fs.split()
 
-# instantiate DLP
+# instantiate DLP client
 dlp_client = dlp_v2.DlpServiceClient()
 
 info_types = [{'name': 'PERSON_NAME'}, {'name': 'DATE_OF_BIRTH'},
@@ -68,7 +70,7 @@ inspect_config = {
     'limits': {'max_findings_per_request': None},
 }
 
-# open and write to DLP CSV file
+# open and write header to DLP CSV file
 with open(dlp_findings, 'w', newline='') as findings:
     out_file = csv.writer(findings, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     out_file.writerow(['Value'] + ['InfoType'] + ['Likelihood'])
@@ -87,13 +89,19 @@ with open(dlp_findings, 'w', newline='') as findings:
         if response.result.findings:
             for finding in response.result.findings:
                 try:
-                    print('Quote: {} | Info type: {} | Likelihood: {}'
-                          .format(finding.quote, finding.info_type.name, finding.likelihood))
                     value = finding.quote
                     info_type = finding.info_type.name
                     likelihood = finding.likelihood
-                    out_file.writerow([value] + [info_type] + [likelihood])
+                    if value in dlp_findings_list:
+                        pass
+                    else:
+                        print('Value: {} | Info type: {} | Likelihood: {}'
+                              .format(value, info_type, likelihood))
+
+                        out_file.writerow([value] + [info_type] + [likelihood])
+                        dlp_findings_list.append(value)
                 except AttributeError as err:
                     print(err)
         else:
+            print('No findings')
             pass
